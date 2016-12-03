e3c043952d7f265f54aa425cfa56c93ae7f793b0
Daniel Hildebrandt <daniel.s.hildebrandt+git@gmail.com>
2016-12-02 16:58:55 -0800

    Added a (mostly) functional local upload button.

---
+++ static/js/local-song-upload-label.js
@@ -0,0 +1,34 @@
+/*
+ * By Osvaldas Valutis, www.osvaldas.info
+ * Available for use under the MIT License
+ */
+
+"use strict";
+
+;(function($, window, document, undefined)
+{
+  $("input.local-song-upload").each(function() {
+    var $input   = $(this),
+        $label   = $input.next("label.local-song-upload-label"),
+        labelVal = $label.html();
+    
+    $input.on("change", function(e) {
+      var fileName = "";
+      
+      if(this.files && this.files.length > 1)
+        fileName = (this.getAttribute("data-multiple-caption") || "").replace("{count}", this.files.length);
+      else if(e.target.value)
+        fileName = e.target.value.split("\\").pop();
+      
+      if(fileName)
+        $label.find("span").html(fileName);
+      else
+        $label.html(labelVal);
+    });
+    
+    // Firefox bug fix
+    $input
+    .on("focus", function(){ $input.addClass("has-focus"); })
+    .on("blur", function(){ $input.removeClass("has-focus"); });
+  });
+})(jQuery, window, document);
