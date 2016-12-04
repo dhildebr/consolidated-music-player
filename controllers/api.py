f52cd79940fb904f17349b3b603ab65520a620f6
Daniel Hildebrandt <daniel.s.hildebrandt+git@gmail.com>
2016-12-03 17:50:03 -0800

    Additional files which failed to be added to last commit.

---
+++ controllers/api.py
@@ -0,0 +1,179 @@
+import requests
+
+# Searching for tracks
+def get_tracks():
+  start_idx = int(request.vars.start_idx) if request.vars.start_idx is not None else 0
+  end_idx = int(request.vars.end_idx) if request.vars.end_idx is not None else 0
+  # We just generate a lot of of data.
+  tracks = []
+  has_more = False
+  rows = db().select(db.track.ALL, limitby=(start_idx, end_idx + 1))
+  # rows.update_record()
+  for i, r in enumerate(rows):
+    if i < end_idx - start_idx:
+      t = dict(
+        id=r.id,
+        artist=r.artist,
+        album=r.album,
+        title=r.title,
+        duration=r.duration,
+        track_source = r.track_source,
+        track_uri = r.track_uri
+      )
+      
+      if r.track_source == 'spotify':
+        t['audio_file'] = "https://embed.spotify.com/?uri={}&theme=white".format(r.track_uri)
+      else:
+        t['audio_file'] = "#"
+      tracks.append(t)
+    else:
+      has_more = True
+  logged_in = auth.user_id is not None
+  return response.json(dict(
+    tracks=tracks,
+    logged_in=logged_in,
+    has_more=has_more,
+  ))
+
+def _get_track_id_from_spotify(track):
+  list=[]
+  url = "https://api.spotify.com/v1/search"
+  params = dict(q="track:"+track, type='track')
+  results = requests.get(url=url, params=params)
+  result_json = results.json()
+  
+  # print result_json
+  if result_json.has_key('tracks'):
+    items = result_json['tracks']['items']
+    # return items
+  else:
+    return None
+  
+  for i in range(0,len(items)):
+    if len(items):
+      list.append(items[i]['id'])
+    else:
+      return None
+  return list
+
+def _get_ids_from_spotify_for_track(track):
+  id_list = _get_track_id_from_spotify(track=track)
+  if id_list is None:
+    response.flash = T("Track '{}' not found".format(track))
+    return []
+  url = "https://api.spotify.com/v1/tracks/?ids="
+  for i in range(0, len(id_list)):
+    url += id_list[i] + ","
+  url = url[:-1]
+  country = 'US'
+  params = {}
+  params['country'] = country
+  results = requests.get(url=url)
+  results_for_db = _parse_spotify_tracks(results.json())
+  return results_for_db
+
+
+def _parse_spotify_tracks(results):
+  tracks = results['tracks']
+  ret_tracks = []
+  # logger.info("Found %d tracks" % len(tracks))
+  for track in tracks:
+    t = {}
+    t['album'] = track['album']['name']
+    t['artist'] = track['artists'][0]['name']
+    t['title'] = track['name']
+    t['duration'] = float(track['duration_ms']) / 1000.0
+    t['track_source'] = 'spotify'
+    t['track_uri'] = track['uri']
+    # logger.info("inserting track %r" % t)
+    # db.track.insert(**t)
+    
+    t['audio_file'] = "https://embed.spotify.com/?uri={}&theme=white".format(t['track_uri'])
+    ret_tracks.append(t)
+  return ret_tracks
+
+
+def add_track_from_spotify():
+  del_songs()
+  tracks = _get_ids_from_spotify_for_track(request.vars.song)
+  tracks_info = []
+  for i in range(0,len(tracks)):
+    t_id = db.track.insert(
+      id = request.vars.id,
+      album = tracks[i]['album'],
+      artist = tracks[i]['artist'],
+      title = tracks[i]['title'],
+      duration = tracks[i]['duration'],
+      track_source = tracks[i]['track_source'],
+      track_uri = tracks[i]['track_uri']
+    )
+    tracks_info.append(t_id)
+  return response.json(dict(track=tracks_info))
+
+def del_songs():
+  db.track.truncate()
+  return "ok"
+
+# --------------------------------
+
+# Add selected track to database
+
+def add_track_to_library():
+  library = []
+  track = db(db.track.id==request.vars.id).select()
+  for t in track:
+    artist = t.artist
+    album = t.album
+    title = t.title
+    duration = t.duration
+    track_source = t.track_source
+    track_uri = t.track_uri
+  t_id = db.library.insert(
+    artist = artist,
+    album = album,
+    title = title,
+    duration = duration,
+    track_source = track_source,
+    track_uri = track_uri
+  )
+  library.append(t_id)
+  response.flash = T(title + " added to library")
+  return response.json(dict(library=library))
+
+def get_library():
+  start_idx = int(request.vars.start_idx) if request.vars.start_idx is not None else 0
+  end_idx = int(request.vars.end_idx) if request.vars.end_idx is not None else 0
+  # We just generate a lot of of data.
+  tracks = []
+  has_more = False
+  rows = db().select(db.library.ALL, limitby=(start_idx, end_idx + 1))
+  # rows.update_record()
+  for i, r in enumerate(rows):
+    if i < end_idx - start_idx:
+      t = dict(
+        id = r.id,
+        artist=r.artist,
+        album=r.album,
+        title=r.title,
+        duration=r.duration,
+        track_source = r.track_source,
+        track_uri = r.track_uri
+      )
+      
+      if r.track_source == 'spotify':
+        t['audio_file'] = "https://embed.spotify.com/?uri={}&theme=white".format(r.track_uri)
+      else:
+        t['audio_file'] = "#"
+      tracks.append(t)
+    else:
+      has_more = True
+  logged_in = auth.user_id is not None
+  return response.json(dict(
+      library=tracks,
+      logged_in=logged_in,
+      has_more=has_more,
+  ))
+
+def del_from_library():
+  db(db.library.id == request.vars.track_id).delete()
+  return "ok"
---
+++ static/js/library.js
@@ -0,0 +1,110 @@
+var app = function() {
+
+    var self = {};
+
+    Vue.config.silent = false;  //show all warnings
+    //
+    //
+    self.extend = function (a, b) {
+        for (var i = 0; i < b.length; i++) {
+            a.push(b[i]);
+        }
+    };
+
+    function get_tracks_url(start_idx, end_idx) {
+        var pp = {
+            start_idx: start_idx,
+                end_idx: end_idx
+        };
+        return library_url + "?" + $.param(pp);
+    }
+
+    self.get_library = function () {
+        $.getJSON(get_tracks_url(0, 5), function (data) {
+            self.vue.library = data.library;
+            self.vue.has_more = data.has_more;
+            self.vue.logged_in = data.logged_in;
+        })
+    };
+
+    self.get_more = function () {
+        var num_tracks = self.vue.library.length;
+        $.getJSON(get_tracks_url(num_tracks, num_tracks + 5), function (data) {
+            self.vue.has_more = data.has_more;
+            self.extend(self.vue.tracks, data.library);
+        });
+    };
+
+    self.delete_track = function (track_id) {
+        $.post(del_track_url,
+            {
+                track_id: track_id
+            },
+            function () {
+                var idx = null;
+                console.log(track_id);
+                for (var i = 0; i < self.vue.library.length; i++) {
+                    if (self.vue.library[i].id === track_id) {
+                        // If I set this to i, it won't work, as the if below will
+                        // return false for items in first position.
+                        idx = i + 1;
+                        break;
+                    }
+                }
+                if (idx) {
+                    self.vue.library.splice(idx - 1, 1);
+                }
+            }
+        )
+    };
+
+    // self.add_track_from_spotify = function () {
+    //     // The submit button to add a track has been added.
+    //     $.post(add_track_from_spotify_url,
+    //         {
+    //             song: self.vue.form_song,
+    //         },
+    //         function (data) {
+    //             $.web2py.enableElement($("#add_track_from_spotify_submit"));
+    //             // for (i = 0; i < data.track.length; i++) {
+    //             //     self.vue.tracks.unshift(data.track[i]);
+    //             // }
+    //             console.log(data.track.length);
+    //         }
+    //     );
+    //     location.reload();
+    // };
+    //
+    // self.add_track_to_library = function (id) {
+    //   $.post(add_track_to_library_url,
+    //       {
+    //           id: id
+    //       },
+    //       function(){
+    //         console.log(id);
+    //       }
+    //   )
+    // };
+    //
+    //
+    self.vue = new Vue({
+        el: "#vue-div",
+        delimiters: ['${', '}'],
+        unsafeDelimiters: ['!{', '}'],
+        data: {
+            library: [],
+            logged_in: false,
+            has_more: false,
+        },
+        methods: {
+            delete_track: self.delete_track
+        }
+    });
+    self.get_library();;
+    $("#vue-div").show();
+    return self;
+
+}
+
+var APP = null;
+jQuery(function() { APP = app(); });
